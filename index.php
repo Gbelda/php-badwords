64686467a6328b8d2d378ce6de97651703644660
gbelda <gbelda56@gmail.com>
2021-12-01 14:59:20 +0100

    Created php file

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+</head>
+<body>
+    <h1>Censor a word by adding "?badword=[word]" in the url </h1>
+
+    <p>
+        The details are not the details. They make the fucking design. Make your work consistent but not fucking predictable. Be fucking bold. 
+        Greatness isn’t the height of your stature, it’s the heft of your spirit. Never, never assume that what you have achieved is fucking good enough. 
+        Paul Rand once said, “The public is more familiar with bad fucking design than good design. It is, in effect, conditioned to prefer bad design,
+         because that is what it lives with. The new becomes threatening, the old reassuring.” You need to sit down and sketch more fucking ideas because 
+         stalking your ex on facebook isn’t going to get you anywhere. What’s important is the fucking drive to see a project through no matter what. 
+         You won’t get good at anything by doing it a lot fucking aimlessly. Creativity is a fucking work-ethic. Never let your guard down by thinking you’re fucking good enough.
+          Your rapidograph pens are fucking dried up, the x-acto blades in your bag are rusty, and your mind is dull. Stop clicking your mouse, get messy, 
+        go back to the basics and make something fucking original. Defy fucking convention. If you don’t test your will against the dissent of
+    </p>
+</body>
+</html>
